655f2cfa8172f7124296df2eadc4bb3dab0cdb67
DevAshleyD <webdevmvp@gmail.com>
2020-10-22 16:30:44 -0400

    launch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import App from 'rmw-shell'
-import React from 'react'
 import config from './config'
 import configureStore from './store'
 
-const Main = () => <App appConfig={{ configureStore, ...config }} />
+const Main = () => < App appConfig = {
+    { configureStore, ...config } }
+/>
 
 export default Main
